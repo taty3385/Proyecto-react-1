8604aabf12ed875ee65ad654d62124e4464f43ba
taty3385 <tatyy661@gmail.com>
2024-03-25 15:02:01 -0300

    agregoo identacion

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -2,13 +2,13 @@
 import List from "./List";
 
 
-export default function ContainerList({ tasks, setTasks ,setDarkMode, darkMode}) {
+export default function ContainerList({ tasks, setTasks, setDarkMode, darkMode }) {
 
-  
+
   return (
     <>
       <List setTasks={setTasks} tasks={tasks} setDarkMode={setDarkMode} darkMode={darkMode} />
-      
-</>
+
+    </>
   );
 }
